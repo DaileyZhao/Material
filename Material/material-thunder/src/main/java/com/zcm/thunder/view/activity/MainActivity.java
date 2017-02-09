c99a6dd7b85cbb2fd9d17b2e27781dc5c8f385a8
zhaocunming <1017355712@qq.com>
2017-02-09 13:12:29 +0800

    drawerlayout

--- Material/material-thunder/src/main/java/com/zcm/thunder/view/activity/MainActivity.java
+++ Material/material-thunder/src/main/java/com/zcm/thunder/view/activity/MainActivity.java
@@ -1,10 +1,18 @@
 package com.zcm.thunder.view.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.zcm.thunder.R;
 
 /**
  * Created by zcm on 2017/1/18.
  */
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity_layout);
+    }
 }
