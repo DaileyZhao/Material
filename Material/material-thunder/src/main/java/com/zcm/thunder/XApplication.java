2a397b218f849b644ce2becb9d93c64be0410dca
DaileyZhao <1017355712@qq.com>
2017-02-26 00:15:41 +0800

    add MultiDex

--- Material/material-thunder/src/main/java/com/zcm/thunder/XApplication.java
+++ Material/material-thunder/src/main/java/com/zcm/thunder/XApplication.java
@@ -1,10 +1,22 @@
 package com.zcm.thunder;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 /**
  * Created by ZCM on 17-2-22 下午4:07.
  */
 
 public class XApplication extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
